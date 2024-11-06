1fa740e74afd918721b7138f31f023e1d6bd8916
priyanshi-furiya <120156568+priyanshi-furiya@users.noreply.github.com>
2024-11-06 12:28:56 +0530

    R031-Decoder

--- model.py
+++ model.py
@@ -4,3 +4,10 @@ def build_seq2seq_model(vocab_size, embedding_dim=128, latent_dim=512):
     encoder_lstm = LSTM(latent_dim, return_state=True)
     encoder_outputs, state_h, state_c = encoder_lstm(encoder_embedding)
     encoder_states = [state_h, state_c]
+
+    decoder_inputs = Input(shape=(None,))
+    decoder_embedding = Embedding(vocab_size, embedding_dim)(decoder_inputs)
+    decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+    decoder_outputs, _, _ = decoder_lstm(decoder_embedding, initial_state=encoder_states)
+    decoder_dense = Dense(vocab_size, activation='softmax')
+    decoder_outputs = decoder_dense(decoder_outputs)
