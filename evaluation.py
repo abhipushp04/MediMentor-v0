4036d42979c2b6d86daad9c2b02b4214f442fb59
shahpruthaa <shahpruthaa@gmail.com>
2024-11-07 15:36:26 +0530

    Update evaluation.py by prutha

--- evaluation.py
+++ evaluation.py
@@ -3,3 +3,13 @@ def decode_sequence(input_seq, encoder_model, decoder_model, tokenizer, max_seq_
     states_value = encoder_model.predict(input_seq)
     target_seq = np.zeros((1, 1))
     target_seq[0, 0] = tokenizer.word_index['<start>']
+    stop_condition = False
+    decoded_sentence = ''
+
+    while not stop_condition:
+        output_tokens, h, c = decoder_model.predict([target_seq] + states_value)
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_word = tokenizer.index_word.get(sampled_token_index)
+
+        if sampled_word:
+            decoded_sentence += ' ' + sampled_word
