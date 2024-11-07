b63903f4b80b246a055172bc32f1d6c48245bf0b
arya2513 <155439832+arya2513@users.noreply.github.com>
2024-11-08 01:41:52 +0530

    R007- decoder

--- model.py
+++ model.py
@@ -35,3 +35,10 @@ def create_inference_models(encoder_inputs, encoder_states, decoder_inputs, deco
     decoder_state_input_h = Input(shape=(latent_dim,))
     decoder_state_input_c = Input(shape=(latent_dim,))
     decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+
+    decoder_outputs, state_h, state_c = decoder_lstm(decoder_embedding, initial_state=decoder_states_inputs)
+    decoder_states = [state_h, state_c]
+    decoder_outputs = decoder_dense(decoder_outputs)
+
+    decoder_model = Model([decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states)
+    return encoder_model, decoder_model
