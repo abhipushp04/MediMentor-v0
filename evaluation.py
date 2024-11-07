66af4f6491e0379aec32bcceaf3d6e6342902135
sannabatra <batrasanna@gmail.com>
2024-11-07 15:37:51 +0530

    Update evaluation.py by sanna

--- evaluation.py
+++ evaluation.py
@@ -13,3 +13,13 @@ def decode_sequence(input_seq, encoder_model, decoder_model, tokenizer, max_seq_
 
         if sampled_word:
             decoded_sentence += ' ' + sampled_word
+            
+        if (sampled_word == '<end>' or len(decoded_sentence) > max_seq_length):
+            stop_condition = True
+
+        target_seq = np.zeros((1, 1))
+        target_seq[0, 0] = sampled_token_index
+        states_value = [h, c]
+
+    return decoded_sentence
+
