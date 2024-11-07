2647b5817861172af2a513ef942915e03b477c48
kairavee-desai <desaikairavee@gmail.com>
2024-11-07 15:32:47 +0530

    Update evaluation.py by kairavee

--- evaluation.py
+++ evaluation.py
@@ -1,1 +1,5 @@
 # Evaluating the model
+def decode_sequence(input_seq, encoder_model, decoder_model, tokenizer, max_seq_length):
+    states_value = encoder_model.predict(input_seq)
+    target_seq = np.zeros((1, 1))
+    target_seq[0, 0] = tokenizer.word_index['<start>']
